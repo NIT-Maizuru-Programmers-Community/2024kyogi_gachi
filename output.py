88f7712e1838765ac91ba94d0aa5965975da2350
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-04-12 22:09:46 +0900

    feat:画面の表示の追加

--- output.py
+++ output.py
@@ -1,2 +1,14 @@
 #output
 
+import sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("simulator")
+
+
+
+ 
+
