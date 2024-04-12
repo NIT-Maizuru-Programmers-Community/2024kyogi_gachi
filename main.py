f62da2d226cb7d6890c6fcee5a1422fba111e402
kawakami-takeru <135945346+kawakami-takeru@users.noreply.github.com>
2024-04-12 16:59:01 +0900

    nukigata追加

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import json
+import threading
+
+def nukigata():
+    for i in range(0,25):
+        
+
+
+
+def output(type,dir):
+
+    write_block=threading.Lock()
+
+    path_B='output.json'
+    action={
+        "actions": [
+          {
+            "type": type,
+            "dir": dir
+          }
+        ]
+      }
+    
+    with write_block:
+        with open(path_B,'w')as f:
+            json.dump(action,f)
