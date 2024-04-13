8b663fcb9a649ad3d7a702bb32a5a064f0a8d92b
kawakami-takeru <135945346+kawakami-takeru@users.noreply.github.com>
2024-04-13 23:26:58 +0900

    num_move関数（未完成）

--- board_reload.py
+++ board_reload.py
@@ -8,7 +8,9 @@ general_patterns_p=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,2
 general_patterns_width=[1,2,2,2,4,4,4,8,8,8,16,16,16,32,32,32,64,64,64,128,128,128,256,256,256]
 general_patterns_height=[1,2,2,2,4,4,4,8,8,8,16,16,16,32,32,32,64,64,64,128,128,128,256,256,256]
 general_patterns_cells=[[[1]],[[1,1],[1,1]],[[1,1],[0,0]],[[1,0],[1,0]],[[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]],
-                        [[1,1,1,1],[0,0,0,0],[1,1,1,1],[0,0,0,0]],[[1,0,1,0],[1,0,1,0],[1,0,1,0],[1,0,1,0]]]
+                        [[1,1,1,1],[0,0,0,0],[1,1,1,1],[0,0,0,0]],[[1,0,1,0],[1,0,1,0],[1,0,1,0],[1,0,1,0]],
+                        [[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1]],
+                        []]
 
 class BoardReload():
 
@@ -19,8 +21,6 @@ class BoardReload():
         self.y=y
         self.s=s
         self.board_now=board_now
-        self.coverd_width=0
-        self.coverd_height=0
         self.flame_board=[]
         self.flame_nukigata=[]
 
@@ -38,30 +38,44 @@ class BoardReload():
                             
                             self.flame_board.append([x,y])
                             self.flame_nukigata.append([i,j])
-                            self.coverd_width=i
-                            self.coverd_height=j
                 
                 print(self.flame_board)
                 print(self.flame_nukigata)
-                print(self.coverd_width)
-                print(self.coverd_height)
 
     def num_move(self):
-         
+        
+        once=True
         self.cover_judge()
 
         if self.s==0:
-            for i in range(len(self.flame_nukigata)):
-              if general_patterns_cells[self.flame_nukigata[i][1]][self.flame_nukigata[i][0]]==1:
+            for i in range(len(self.flame_nukigata)-1,-1,-1):
+              if general_patterns_cells[self.p][self.flame_nukigata[i][1]][self.flame_nukigata[i][0]]==1:
+                   y=self.flame_board[i][1]
+                   x=self.flame_board[i][0]
+                   if once==True:
+                        tmp=board_height-y
+                        once=False
+                   for j in range(1,tmp):
+                        board_now[y+j-1][x],board_now[y+j][x]=board_now[y+j][x],board_now[y+j-1][x]
+
         elif self.s==1:
-            
-        elif self.s==2:
+            for i in range(0,len(self.flame_nukigata)):
+              if general_patterns_cells[self.p][self.flame_nukigata[i][1]][self.flame_nukigata[i][0]]==1:
+                   y=self.flame_board[i][1]
+                   x=self.flame_board[i][0]
+                   if once==True:
+                        tmp=y
+                        once=False
+                   for j in range(0,tmp):
+                        board_now[y+j][x],board_now[y+j-1][x]=board_now[y+j][x],board_now[y+j-1][x]
+        # elif self.s==2:
                 
-        elif self.s==3:
+        # elif self.s==3:
                          
 
-board_reload=BoardReload(7,-1,-1,0,board_now)
-board_reload.cover_judge()
+board_reload=BoardReload(1,0,2,1,board_now)
+board_reload.num_move()
+print(board_now)
          
 
 
