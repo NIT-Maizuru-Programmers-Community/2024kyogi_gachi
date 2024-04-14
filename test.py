fe463e129017a395eeacc74c8c69ff2b98769c9c
fusakk <fusakk1@gmail.com>
2024-04-14 15:54:18 +0900

    途中経過1

---
+++ test.py
@@ -0,0 +1,48 @@
+board_width=6
+board_height=4
+board_start=[[2,2,0,1,0,3],[2,1,3,0,3,3],[0,2,2,1,0,3],[3,2,2,0,3,3]]
+board_now=[[2,2,0,1,0,3],[2,1,3,0,3,3],[0,2,2,1,0,3],[3,2,2,0,3,3]]
+board_goal=[[0,0,0,0,0,0],[1,1,1,2,2,2],[2,2,2,2,3,3],[3,3,3,3,3,3]]
+
+general_patterns_p=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+general_patterns_width=[1,2,2,2,4,4,4,8,8,8,16,16,16,32,32,32,64,64,64,128,128,128,256,256,256]
+general_patterns_height=[1,2,2,2,4,4,4,8,8,8,16,16,16,32,32,32,64,64,64,128,128,128,256,256,256]
+general_patterns_cells=[[[1]],[[1,1],[1,1]],[[1,1],[0,0]],[[1,0],[1,0]],[[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]],
+                        [[1,1,1,1],[0,0,0,0],[1,1,1,1],[0,0,0,0]],[[1,0,1,0],[1,0,1,0],[1,0,1,0],[1,0,1,0]],
+                        [[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1]],
+                        []]
+
+class BoardReload():
+
+    def __init__(self,p,x,y,s,board_now):
+
+        self.p=p
+        self.x=x
+        self.y=y
+        self.s=s
+        self.board_now=board_now
+        self.flame_board=[]
+        self.flame_nukigata=[]
+        self.cover_judge()
+
+    def cover_judge(self):
+                
+                x=self.x
+                y=self.y
+                for i in range(0,general_patterns_width[self.p]):
+                    x=self.x+i
+                    y=self.y
+                    for j in range(0,general_patterns_height[self.p]):
+                        y=self.y+j
+                        if x<board_width and y<board_height and x>=0 and y>=0:
+                            self.flame_board.append([x,y])
+                            self.flame_nukigata.append([i,j])
+
+    def num_move(self):
+        if(self.s==0):
+             nukigata_width = [i for i in self.flame_board if i[0] == 0]
+        print(result)
+
+board_reload=BoardReload(1,0,0,0,board_now)
+board_reload.num_move()
+print(board_reload.cover_judge())
