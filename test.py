5f2cef5780b0c55c5e7074d4f7a31ed590e29249
fusakk <fusakk1@gmail.com>
2024-04-15 00:35:20 +0900

    cover_judge完成

--- test.py
+++ test.py
@@ -21,42 +21,34 @@ class BoardReload():
         self.y=y
         self.s=s
         self.board_now=board_now
-        self.flame_board=[]
-        self.flame_nukigata=[]
-        self.cover_judge()
+        self.nukigata_width=0
+        self.nukigata_height=0
+        self.nukigata_x=0
+        self.nukigata_y=0
 
     def cover_judge(self):
-                
-                x=self.x
-                y=self.y
-                for i in range(0,general_patterns_width[self.p]):
-                    x=self.x+i
-                    y=self.y
-                    for j in range(0,general_patterns_height[self.p]):
-                        y=self.y+j
-                        if x<board_width and y<board_height and x>=0 and y>=0:
-                            self.flame_board.append([x,y])
-                            self.flame_nukigata.append([i,j])
+        if(self.x<0):
+            self.nukigata_x=0
+        else:self.nukigata_x=self.x
+        if(self.y<0):
+            self.nukigata_y=0
+        else:self.nukigata_y=self.y
+        
+        if(self.x+general_patterns_width[self.p]<=board_width):
+            if(self.x<0):
+                self.nukigata_width=self.x+general_patterns_width[self.p]
+            else:self.nukigata_width=general_patterns_width[self.p]
+        else:self.nukigata_width=board_width-self.x
 
-    def num_move(self):
-        nuki=self.flame_board
-        nuki_line=int
-        depth=int
+        if(self.y+general_patterns_height[self.p]<=board_height):
+            if(self.y<0):
+                self.nukigata_height=self.y+general_patterns_height[self.p]
+            else:self.nukigata_height=general_patterns_height[self.p]
+        else:self.nukigata_height=board_height-self.y
 
-        if(self.s==0):
-             nuki_line = [i for i in self.flame_board if i[0] == self.flame_board[0][0]]
-             nuki_depth = [i for i in self.flame_board if i[1] == self.flame_board[0][1]]
-             depth=board_height-self.flame_board[0][0]
-             move_count=[]*len(nuki_line)
-             tyousei=self.flame_board[0]
-             for i in range(nuki_line[0][1],nuki_line[len(nuki_line)-1][1]+1): #(列)回繰り返す
-                   for j in range(nuki_depth[0][0],nuki_depth[len(nuki_depth)-1][0]+1): #(深さ)回繰り返す
-                        if(general_patterns_cells[self.p][i-tyousei[0]][j-tyousei[1]]==1):
-                             move_count[i]=3
-                             print(move_count)
-                             
-                  
+        print(self.nukigata_x,self.nukigata_y,self.nukigata_width,self.nukigata_height) #抜き型のx,y,width,height
+            
+    #def num_move(self):
 
-board_reload=BoardReload(1,1,1,0,board_now)
-board_reload.num_move()
-#print(board_reload.cover_judge())
+board_reload=BoardReload(1,0,0,0,board_now)
+board_reload.cover_judge()
