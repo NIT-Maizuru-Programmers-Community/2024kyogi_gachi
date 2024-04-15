b23be8db484b0307423f8eeff435103fb65a4f7d
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-04-16 03:39:28 +0900

    feat:classの追加

---
+++ simu_main.py
@@ -0,0 +1,22 @@
+import judge
+
+seikai=[[1,2,3],[3,2,2],[1,2,2]]
+relord=[[1,2,2],[1,2,2],[1,3,2]]
+
+
+class simu(judge.judge):
+    def kari(self):
+        print("kyogi")
+
+
+seigo=simu()
+seigo.kari()
+#print(seigo.judges(relord,seikai))
+
+
+
+
+
+
+
+
