6db774fade333d23db02ba7f5f5f2a8830ed5c5a
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-04-16 23:57:37 +0900

    ファイル名変えてみる

--- simu_main.py
+++ simu_main.py
@@ -1,10 +1,12 @@
-import judge
+from judge import judgec
 
 seikai=[[1,2,3],[3,2,2],[1,2,2]]
 relord=[[1,2,2],[1,2,2],[1,3,2]]
 
 
-class simu(judge.judge):
+
+
+class simu(judgec):
     def kari(self):
         print("kyogi")
 
