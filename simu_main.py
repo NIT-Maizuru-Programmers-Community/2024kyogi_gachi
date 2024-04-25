86ba88e37edef1eef021414fe2fdb99ebd37e596
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-04-25 16:13:58 +0900

    feat:インデント見やすく

--- simu_main.py
+++ simu_main.py
@@ -13,7 +13,6 @@ class simu(judge.Judgec,board_reload_fujii.BoardOperation,algorithm.karial):
         self.correct_board=[[1,2,3],[1,2,3],[1,2,3]]#正解の盤面
         self.now_board=[[1,1,1],[2,2,2],[3,3,3]]#現在の盤面
         self.use_type=general_patterns.general_patterns_cells.copy()#使用できる抜き型
-
         self.start=start_time
         self.turn=turn
 
@@ -30,15 +29,12 @@ class simu(judge.Judgec,board_reload_fujii.BoardOperation,algorithm.karial):
         self.end = self.get_time()
 
         self.cutter_position=[self.call_algotithm[1],self.call_algotithm[2]]#使用した座標
-
         self.relord_board=self.board_update(self.call_algotithm[0],self.cutter_position,self.call_algotithm[3],self.now_board)
         #処理後の盤面取得(use_type,zahyou,move_direc,before_board)
-
         self.correct=self.judge(self.relord_board,self.correct_board)#正誤判定
 
         self.now_board=self.relord_board.copy()#盤面書き換え
         self.times=self.end-self.start#実行時間
-
 
         output.log_output(self.relord_board,self.turn,self.times,self.call_algotithm[0],self.cutter_position,self.call_algotithm[3],self.correct[1])
         #relord_board,turn,time,use_type,use_coodenate,move_direc,TF
@@ -47,35 +43,8 @@ class simu(judge.Judgec,board_reload_fujii.BoardOperation,algorithm.karial):
              sys.exit()
 
 
-
-
 start_time = time.time()#開始時間
 simul=simu()
 
 for i in range(1,20):
     simul.set(start_time,i)
-
-
-
-
-
-    
-
-
-
-
-
-
-#output.log_output(relord_board,turn,time,use_type,use_coodenate,move_direc,TF)
-
-
-
-
-
-
-
-
-
-
-
-
