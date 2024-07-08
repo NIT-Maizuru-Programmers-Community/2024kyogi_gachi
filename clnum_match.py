6df50d4d6c9c6de676519abf588fc378b69c3cf6
kawakami-takeru <135945346+kawakami-takeru@users.noreply.github.com>
2024-07-08 16:02:52 +0900

    無理かも

--- clnum_match.py
+++ clnum_match.py
@@ -1,4 +1,3 @@
-dir=0 #0:上 1:下 2:左 3:右
 layer=0 #n層目
 goal_board=[[1,2,3,2,1],
             [2,3,1,1,2],
@@ -12,30 +11,38 @@ now_board=[[3,2,3,1,2],
            [2,1,3,2,3],
            [1,1,3,0,1]]
 
-def fitnum(now_board,goal_board,dir,layer,wide,height):
-    now_num0=0
-    now_num1=0 #1の個数
-    now_num2=0 #2の個数
-    now_num3=0 #3の個数
-    goal_num0=0
-    goal_num1=0 
-    goal_num2=0 
-    goal_num3=0
+def fitnum(now_board,goal_board,layer,wide,height):
+    now_0=0 #0の数
+    now_1=0 #1の数
+    now_2=0 #2の数
+    now_3=0 #3の数
+    goal_0=0
+    goal_1=0 
+    goal_2=0 
+    goal_3=0
     for i in range(wide):
-        if now_board[layer,i]==0:
-            now_num0+=1
-        if now_board[layer,i]==1:
-            now_num1+=1
-        if now_board[layer,i]==2:
-            now_num2+=1
-        if now_board[layer,i]==3:
-            now_num3+=1
-        if goal_board[layer,i]==0:
-            goal_num0+=1
-        if goal_board[layer,i]==1:
-            goal_num1+=1
-        if goal_board[layer,i]==2:
-            goal_num2+=1
-        if goal_board[layer,i]==3:
-            goal_num3+=1
-    
+        if now_board[0][i]==0:
+            now_0+=1
+        if now_board[0][i]==1:
+            now_1+=1
+        if now_board[0][i]==2:
+            now_2+=1
+        if now_board[0][i]==3:
+            now_3+=1
+        if goal_board[0][i]==0:
+            goal_0+=1
+        if goal_board[0][i]==1:
+            goal_1+=1
+        if goal_board[0][i]==2:
+            goal_2+=1
+        if goal_board[0][i]==3:
+            goal_3+=1
+
+    new_0=now_0-goal_0
+    new_1=now_1-goal_1
+    new_2=now_2-goal_2
+    new_3=now_3-goal_3
+
+    for i in range(wide):
+        if now_board[0][i]==0:
+            if new_0>0
