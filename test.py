a5da54406f36448d86d9ad2e53e6f999f0b773b6
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-07-09 22:24:16 +0900

    update:努力・未来・Beautifulstar

--- test.py
+++ test.py
@@ -5,23 +5,12 @@ goal_board=[[1,2,3,2,1],
             [2,1,3,0,1],
             [2,1,3,0,3]]#0:3,1:9,2:9,3:9
 
-a=3
-if a % 2==1:
-    print("h偶数")
 
-now_match_position=7
-send_position=2
-c=(now_match_position-send_position)//2 + (now_match_position-send_position)%2
-print(c)
-
-
-
-ar=[]
-ar.extend([])
-print(ar)
 board_row=[list(x) for x in zip(*goal_board)]#行で参照のため転地
 
 print(board_row)
+
+for colum in range(len(goal_board)):
 
 #print(get_num(goal_board))
 #tuple = [list(x) for x in zip(*goal_board)]#転地
