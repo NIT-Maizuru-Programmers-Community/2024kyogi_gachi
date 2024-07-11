8f197f2c579bf1e1983ee0073a694cf1564df4bb
kawakami-takeru <135945346+kawakami-takeru@users.noreply.github.com>
2024-07-12 00:19:23 +0900

    移動以外はインデックス取得するとこまでできた

--- clnum_match.py
+++ clnum_match.py
@@ -1,4 +1,6 @@
-layer=0 #n層目
+layer=1 #n層目
+wide=5
+height=5
 goal_board=[[1,2,3,2,1],
             [2,3,1,1,2],
             [2,1,3,0,2],
@@ -12,37 +14,50 @@ now_board=[[3,2,3,1,2],
            [1,1,3,0,1]]
 
 def fitnum(now_board,goal_board,layer,wide,height):
-    now_0=0 #0の数
-    now_1=0 #1の数
-    now_2=0 #2の数
-    now_3=0 #3の数
-    goal_0=0
-    goal_1=0 
-    goal_2=0 
-    goal_3=0
-    for i in range(wide):
-        if now_board[0][i]==0:
-            now_0+=1
-        if now_board[0][i]==1:
-            now_1+=1
-        if now_board[0][i]==2:
-            now_2+=1
-        if now_board[0][i]==3:
-            now_3+=1
-        if goal_board[0][i]==0:
-            goal_0+=1
-        if goal_board[0][i]==1:
-            goal_1+=1
-        if goal_board[0][i]==2:
-            goal_2+=1
-        if goal_board[0][i]==3:
-            goal_3+=1
 
-    new_0=now_0-goal_0
-    new_1=now_1-goal_1
-    new_2=now_2-goal_2
-    new_3=now_3-goal_3
+    now_count=[0,0,0,0]  #現在の盤面におけるそれぞれの数字の数
+    goal_count=[0,0,0,0] #正解の盤面におけるそれぞれの数字の数
+    for i in range(wide): #各盤面における数字の数をカウント
+        for j in range(4):
+            if now_board[layer][i]==j:
+                now_count[j]+=1
+        for j in range(4):
+            if goal_board[layer][i]==j:
+                goal_count[j]+=1
+    
+    evalution_value=[0,0,0,0] #評価値
+    for i in range(4): #過分、不足、満足を評価
+        evalution_value[i]=now_count[i]-goal_count[i]
+    
+    excess_index=[layer,0] #過分のインデックス
+    for k in range(4): #過分のインデックス取得
+        if evalution_value[k]>0:
+            excess_index[1]=now_board[layer].index(k)
+            break
+    
+    shortage_index=[0,0] #不足のインデックス
+    for k in range(4): #不足のインデックス取得
+        if evalution_value[k]<0:
+            for i in range(layer+1,height):
+                for j in range(wide):
+                    if now_board[i][j]==k:
+                        shortage_index[0]=i
+                        shortage_index[1]=j
+                        break
+                else:
+                    continue
+                break
+            else:
+                continue
+            break
+            
 
-    for i in range(wide):
-        if now_board[0][i]==0:
-            if new_0>0
+
+
+    
+    return [now_count,goal_count,evalution_value,excess_index,shortage_index]
+
+
+ans=fitnum(now_board,goal_board,layer,wide,height)
+
+print(ans)
