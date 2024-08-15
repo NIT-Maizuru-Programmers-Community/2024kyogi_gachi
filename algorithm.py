8fc592c2e985c0db90257d22b91dde1c09b8fd84
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-08-15 15:25:31 +0900

    feat:時間の取得処理

--- algorithm.py
+++ algorithm.py
@@ -3,8 +3,7 @@
 #出力は(抜き型の番号,使用する座標(x),使用する座標(y),詰める方向)がよいよ
 
 class karial:
-    start_board = [[0, 1], [2, 3]]
-    correct_board = [[2, 1], [0, 3]]
+    
     
     def algo(self,now_board,correct_board,cut_type):
         
