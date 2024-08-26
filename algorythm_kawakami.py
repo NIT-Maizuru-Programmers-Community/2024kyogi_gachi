3eddb9404e2d93b9aea16ddb70afef98c41a38f7
kawakami-takeru <135945346+kawakami-takeru@users.noreply.github.com>
2024-08-26 20:03:11 +0900

    新しく作った

---
+++ algorythm_kawakami.py
@@ -0,0 +1,29 @@
+import judge
+import board_reload_fujii
+import general_patterns
+
+start_board=[[0,2,0,3,1],
+           [3,1,1,1,0],
+           [0,2,0,2,3],
+           [1,2,0,0,3],
+           [2,3,1,2,3]]
+
+correct_board=[[0,0,0,0,0],
+               [0,0,1,1,1],
+               [1,1,1,2,2],
+               [2,2,2,2,3],
+               [3,3,3,3,3]]
+
+height=5
+wide=5
+
+class algo_kawakami():
+    def __init__(self,start_board,correct_board,hight,wide):
+        self.start=start_board
+        self.correct=correct_board
+        self.hight=hight
+        self.wide=wide
+
+    def 
+
+print("test")
