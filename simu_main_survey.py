eeef26069e005822c51f889c736e35307c93b82a
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-08-30 23:19:39 +0900

    feat/手数を見やすく

--- simu_main_survey.py
+++ simu_main_survey.py
@@ -10,10 +10,10 @@ import numpy as np
 class simu(judge.Judgec,algorithm_survey.algorithm_tentative,board_reload_fujii.BoardOperation):
 
     def set(self):
-        first_board = np.random.randint(0, 4, (50, 50))
+        first_board = np.random.randint(0, 4, (150, 150))
         self.correct_board=first_board.tolist() #正解の盤面
         shuffled_elements = np.random.permutation(first_board.flatten())
-        second_board = shuffled_elements.reshape(50, 50)
+        second_board = shuffled_elements.reshape(150, 150)
         self.now_board=second_board.tolist() #現在の盤面
         self.use_type=general_patterns.general_patterns_cells.copy()#使用できる抜き型
 
@@ -42,18 +42,12 @@ class simu(judge.Judgec,algorithm_survey.algorithm_tentative,board_reload_fujii.
         for turn in range(1,len(self.call_algotithm[0])+1):
             #self.end = self.get_time()
             self.turn_algorithm=self.call_algotithm[0][turn-1]#そのターンの操作
-
             self.cutter_position=[self.turn_algorithm[1],self.turn_algorithm[2]]#使用した座標
-
             self.relord_board=self.board_update(self.turn_algorithm[0],self.cutter_position,self.turn_algorithm[3],self.now_board)
             #処理後の盤面取得( cutter_num, cutter_LU_posi, move_direction, board):
             #print(f"{self.relord_board}self.relord_board")
-
             self.correct=self.judge(self.relord_board,self.correct_board)#正誤判定
-
             self.now_board=self.relord_board.copy()#盤面書き換え
-            
-
             output.log_output(self.relord_board,turn,self.time,self.turn_algorithm[0],self.cutter_position,self.turn_algorithm[3],self.correct[1])
             #relord_board,turn,time,use_type,use_coodenate,move_direc,TF
 
