365a8eb99a7e24c002480a062f59527ad262ec18
kouki <koukifujii0512@gmail.com>
2024-09-05 05:58:40 +0900

    feat: 一層分の全探索プログラム
    評価値が変化せずにループする現象がある

--- algorithms/algorithm_fujii.py
+++ algorithms/algorithm_fujii.py
@@ -6,6 +6,7 @@ import os
 # 相対パスを使用して一つ上の階層のパスを追加
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+import copy
 import numpy as np
 import board_reload_fujii
 import judge as J
@@ -16,11 +17,11 @@ class MyAlgo:
         self.width = board_width
         self.special_cutter_quantity = special_cutter_quantity
         self.board_op = board_reload_fujii.BoardOperation()
+        self.log = []
     
     def algo(self, now_board, correct_board):
-        self.now = now_board
+        self.now = copy.deepcopy(now_board)
         self.correct = correct_board
-        self.log = []
 
         def all_position():
             #すべての座標を探索
@@ -28,11 +29,12 @@ class MyAlgo:
             for y in range(self.height):
                 for x in range(self.width):
                     min_distance_data =  self.force_search(x, y)
+                    #print("min_distance_data=", min_distance_data)
                     if recommended_action[0] > min_distance_data[0]:
                         recommended_action[0] = min_distance_data[0]
-                        recommended_action[1] = min_distance_data[1]
-                        recommended_action[2] = x ,y
-                        recommended_action[3] = min_distance_data[2]
+                        recommended_action[1] = min_distance_data[1] #cutter_num
+                        recommended_action[2] = [x ,y]
+                        recommended_action[3] = min_distance_data[2] #direction
             return [recommended_action[1], recommended_action[2], recommended_action[3]]
         
         recommended_action = all_position()
@@ -40,23 +42,26 @@ class MyAlgo:
 
         return recommended_action
 
+    #ある座標に対してスコアと最適な型と方向を求める
     def force_search(self, x, y):
         min_distance_data = [9999999, 0, 0] #distance, cutter_num, move_direction
         for cutter_num_1 in range(25 + self.special_cutter_quantity):
             for move_direction in range(4):
-                next_board = self.board_op.board_update(cutter_num_1, [x, y], move_direction, self.now)
+                next_board = self.board_op.board_update(cutter_num_1, [x, y], move_direction, copy.deepcopy(self.now))
                 total_distance = self.evaluation(next_board)
+
                 if min_distance_data[0] > total_distance:
                     min_distance_data[0] = total_distance
                     min_distance_data[1] = cutter_num_1
                     min_distance_data[2] = move_direction
         return min_distance_data
 
+    #すべてのセルの正解との距離を加算
     def evaluation(self, board):
         def find_closest_equal_value(board, target_value, x, y):
             # ターゲット値と等しいすべてのインデックスを取得
-            indices = np.argwhere(board == target_value) 
-            min_manhattan_distance = 999
+            indices = [(i, j) for i in range(len(board)) for j in range(len(board[i])) if board[i][j] == target_value]
+            min_manhattan_distance = float('inf')
 
             for idx in indices:
                 manhattan_distance = abs(idx[1] - x) + abs(idx[0] - y)
@@ -76,8 +81,8 @@ class MyAlgo:
 
 
 def main():
-    start_board = [[0, 1], [2, 3]]
-    correct_board = [[2, 1], [0, 3]]
+    start_board = [[0, 1, 2, 1], [3, 0, 1 ,0], [2, 3, 0, 2], [2, 3, 0, 2]]
+    correct_board = [[0, 2, 0, 2], [2, 3, 0, 2], [1, 3, 1, 0], [2, 0, 1, 3]]
     height = len(start_board)
     width = len(start_board[0])
     my_algo = MyAlgo(height, width, 0)
@@ -88,15 +93,21 @@ def main():
     incorrect_total = []
     board = start_board
     cnt = 1
+
     while judge_result != True:
-        print(cnt)
+        print("cnt=", cnt)
         recommended_action = my_algo.algo(board, correct_board)
-        board = board_op.board_update(recommended_action[0], recommended_action[1], recommended_action[2], board)
+        print("recomm!=", recommended_action)
+        cutter = recommended_action[0]
+        posi = recommended_action[1]
+        direction = recommended_action[2]
+        board = board_op.board_update(cutter, posi, direction, board)
+        print("board=", board)
         judge_result, incorrect = judge.judge(board, correct_board)
         incorrect_total.append(incorrect)
         cnt += 1
     
-    print(my_algo.log)
-    print(incorrect_total)
+    print("log=", my_algo.log)
+    print("incorrect_total=", incorrect_total)
 
 main()
