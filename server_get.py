01663d7b5399682ee033c7721dd612ca3efd0e67
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-09-20 14:18:05 +0900

    feat/serverとの通信

---
+++ server_get.py
@@ -0,0 +1,16 @@
+import requests
+import pprint
+
+# リクエストパラメータを辞書型で設定する
+param = {"token": "token1"}
+
+# get()メソッドでGETリクエストを送信する
+response = requests.get("http://localhost:8080/problem", params=param)
+
+get_data=vars(response)
+
+
+print(get_data)
+
+# リクエスト時のURLを出力する
+#pprint.pprint(vars(response))
--- test.py
+++ test.py
@@ -1,20 +1,6 @@
 
 now_board_layer=[2,3,5,1,6,3,4]
-goal_num=6
-place=1
-
-now_count=[2,3,5,1,6,3,4]
-goal_count=[2,3,5,1,6,3,4]
-#goal_count=[1,3,6,2,5,3,4]
 
 
 
-while(now_board_layer[place+1]!=goal_num):
-            place=place+1
-            
-        
-print(place+1)
-
-while now_count!=goal_count:
-        print("え")
-        
+key1=["name","age","kind"]
