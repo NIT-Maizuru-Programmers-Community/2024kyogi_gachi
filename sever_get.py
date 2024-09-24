8ff9cab64e4600cc388f8439728efd80cf7d8095
w.s <watanabesenhsn@gmail.com>
2024-09-24 16:15:30 +0900

    Update sever_get.py

--- sever_get.py
+++ sever_get.py
@@ -7,5 +7,8 @@ param = {"token": "token1"}
 # get()メソッドでGETリクエストを送信する
 response = requests.get("http://localhost:8000/problem", params=param)
 
+
+get_data = vars(response) 
 # リクエスト時のURLを出力する
-pprint.pprint(vars(response))
+#pprint.pprint(vars(response))
+print(get_data)
