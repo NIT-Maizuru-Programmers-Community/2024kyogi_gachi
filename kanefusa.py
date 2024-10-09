f398fd1dc3b85273ac499cd940f8aed974165cd6
fusakk <fusakk1@gmail.com>
2024-10-09 16:18:33 +0900

    ダブりなくす

--- kanefusa.py
+++ kanefusa.py
@@ -1,5 +1,5 @@
 cutter_scale_array=[128,64,32,16,8,4,2,1]
-cloce_distance=31 #間隔（幅）
+cloce_distance=19 #間隔（幅）
 
 div_cloce_distance=cloce_distance #div_cloce_distanceを、定型(cutter_scale_array)で分割
 composition_list=[] #dic_cloce_distanceを構成する数字(定型)を格納
@@ -21,7 +21,8 @@ for i in range((1<<len(composition_list))):
     num=0
     for j in range(len(composition_list)):
         if((i>>j)&1):
-            num+=composition_list[j]
+            if((str(bin(i)).count('1'))!=1):
+                num+=composition_list[j]
     
     #00...0の場合は考えない(00...1 ~ 11...1)
     if(num!=0):
