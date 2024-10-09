468db0498f191a22824a3a8567dabcf42c739ad6
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-10-09 15:10:36 +0900

    improvement/コメントの削除

--- algorithm_general.py
+++ algorithm_general.py
@@ -34,15 +34,18 @@ class algorithm_tentative(board_reload_fujii.BoardOperation):
         for column in range(0,len(self.now_board)):
             #print(f"{column}層目")
 
-            #一致度高いやつ寄せる
-            self.array_operation=array_send.column_row_send(self.operation_board,self.goal_board,column)
+
+            self.array_operation=array_send.column_row_send(self.operation_board,self.goal_board,column)#一致度高いやつ寄せる
             self.array_operate_board.extend(self.array_operation)
-            #ボードの更新
-            for turn_num in range(0,len(self.array_operation)):
+            #print(f"{self.array_operation}#一致度高いやつ寄せる")
+
+            
+            #print(len(self.array_execution_time))
+
+            for turn_num in range(0,len(self.array_operation)):#ボードの更新
                 self.array_operation_position=[self.array_operation[turn_num][1],self.array_operation[turn_num][2]]
                 self.operation_board=self.board_update(self.array_operation[turn_num][0], self.array_operation_position, self.array_operation[turn_num][3], self.operation_board)        
             #print(f"{self.operation_board}#一致度高いやつ寄せる盤面")
-
 
 
             #各要素の個数をそろえる
@@ -51,13 +54,22 @@ class algorithm_tentative(board_reload_fujii.BoardOperation):
             self.array_operate_board.extend(self.element_operation[0])
             #print(f"{self.element_operation}#各要素の個数をそろえる")
             self.operation_board=copy.deepcopy(self.element_operation[1])
+
+            
             #print(f"{self.operation_board}#各要素の個数をそろえる盤面")
 
+            self.match_operation=clmatch_cutting.clmatch(self.operation_board,self.goal_board,column,self.wide)#順番を一致させる
+            #self.match_operation=clmatch.clmatch(self.operation_board,self.goal_board,column,self.wide,self.height)#順番を一致させる
 
-            #順番を一致させる
-            self.match_operation=clmatch_cutting.clmatch(self.operation_board,self.goal_board,column,self.wide)
+            
+            # print(f"{len(self.array_execution_time)}self.match_operation")
+            # print(f"{len(self.match_operation[0])}リストながさ")
+
+
             self.array_operate_board.extend(self.match_operation[0])
+            
             self.operation_board=copy.deepcopy(self.match_operation[1])
+
             #print(f"{self.operation_board}#順番を一致させる盤面")
 
         #print(self.array_operate_board)
