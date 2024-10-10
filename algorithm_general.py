02de2ba3fc579d9518b1c076189e41d136751fd6
e9405 <e9405@g.maizuru-ct.ac.jp>
2024-10-10 18:17:39 +0900

    feat/一般抜型を使える形にする部分完成

--- algorithm_general.py
+++ algorithm_general.py
@@ -27,57 +27,94 @@ class algorithm_tentative(board_reload_fujii.BoardOperation):
         self.wide=width
         self.array_operate_board=[]#ここに操作を追加
         self.operation_board = copy.deepcopy(self.now_board)
+        cutter_scale_array=[258,128,64,32,16,8,4,2,1]
         just_type=[]#[抜き型番号,詰めれる距離,削った距離]
         general_usable=[]#[抜き型番号,幅,詰めれる距離,削った距離]
 
 
 
-        #一般抜き型を使用できるように
-        #just_typeを作成
-        for just in range(25,len(cut_type)):
-            just_cut=cut_type[just][0]
-            cutter_distance=0
-            sharpen_distance=0
+        #一般を使える形に
+        for general_num in range(25,len(cut_type)): #一般は25から
+
+            general_cut=cut_type[general_num][0]#今回のループの一番上
+            cutter_distance=0#詰める距離
+            sharpen_distance_left=0#左側の削る距離
+            sharpen_distance_right=0#右側の削る距離
+            between_count=0#間の距離
+            general_distance=cutter_distance+between_count#幅
             is_exist=False
-            for sharpen in range(0,len(just_cut)):#削る距離カウント
-                print(sharpen)
-                if just_cut[sharpen]==1:
+            is_exist_just=False
+
+            for sharpen in range(0,len(general_cut)):#左側の削る距離カウント
+                if general_cut[sharpen]==1:
                     break
-                sharpen_distance+=1
-
-            if sharpen_distance==len(just_cut):#上全部が0の場合飛ばす
-                continue
-
-            for potential in range(sharpen_distance,len(just_cut)):#詰めれる距離カウント
-                if just_cut[potential]==0:
-                    break
-                cutter_distance+=1
-
-            for cutter in range(0,len(just_type)):
-                if just_type[cutter][1]==cutter_distance:
-                    is_exist=True
-
-            if is_exist==False:
-                just_type.append([just,cutter_distance,sharpen_distance])
-
-        #general_usableを作成
-        for just in range(0,len(cut_type)):
-            general_cut=cut_type[just][0]
-            cutter_distance=0
-            sharpen_distance=0
-            is_exist=False
-            for sharpen in range(0,len(just_cut)):#削る距離カウント
-                print(sharpen)
-                if just_cut[sharpen]==1:
-                    break
-                sharpen_distance+=1
+                sharpen_distance_left+=1
             
-            if sharpen_distance==len(just_cut):#上全部が0の場合飛ばす
+            if sharpen_distance_left==len(general_cut):#上全部が0の場合飛ばす
                 continue
 
+            for sharpen in reversed(general_cut):#右側の削る距離カウント
+                if sharpen==1:
+                    break
+                sharpen_distance_right+=1
 
+            while general_distance+sharpen_distance_left!=len(general_cut)-sharpen_distance_right:#左の削る距離+詰める距離+間の距離==抜き型の大きさ-右側の削る距離
+                is_exist=False#while内部でのis_existの再定義
+                is_exist_just=False
 
+                for cutter in range(general_distance+sharpen_distance_left,len(general_cut)-sharpen_distance_right):#詰めれる距離カウント
+                    if general_cut[cutter]==0:
+                        break
+                    cutter_distance+=1
+                general_distance=cutter_distance+between_count
+                
+                if general_distance!=cutter_distance:#general_usableを追加
+                    for search in range(0,len(general_usable)):#同じ長さのやつがないか探す
 
+                        for i in range(0,len(cutter_scale_array)):#定型と同じ距離詰めるなら省く
+                            if cutter_scale_array[i]==cutter_distance:
+                                is_exist=True
+                                break
+                        
+                        if is_exist==True:
+                            break
+                        
+                        if general_usable[search][2]==cutter_distance:#幅詰める距離が同じ場合、幅がより小さいものを取得
+                            is_exist=True
+                            if general_usable[search][1] > general_distance:
+                                general_usable[search]=[general_num,general_distance,cutter_distance,sharpen_distance_left]
+                            else:
+                                break
+                    
+                    if is_exist==False:
+                        general_usable.append([general_num,general_distance,cutter_distance,sharpen_distance_left])
+                
+                else:
+                    #just_typeを追加
+                    
+                    print(f"{cutter_distance}cutter_distance")
+                    for i in range(0,len(cutter_scale_array)):#定型と同じ距離詰めるなら省く
+                        if cutter_scale_array[i]==cutter_distance:
+                            is_exist_just=True
+                            print(f"{cutter_distance}cutter_distance同じだよ")
+                            break
+                    
+                    if is_exist==True:    
+                        for search in range(0,len(just_type)):#同じ長さのやつがないか探す
+                            if just_type[search][1]==cutter_distance:
+                                is_exist_just=True
+                                break
+
+                    if is_exist_just==False:
+                        just_type.append([general_num,cutter_distance,sharpen_distance_left])
+
+            
+                for between in range(general_distance+sharpen_distance_left,len(general_cut)-sharpen_distance_right):#間カウント
+                    if general_cut[between]==1:
+                        break
+                    between_count+=1
+                general_distance=cutter_distance+between_count
+                
 
 
         
