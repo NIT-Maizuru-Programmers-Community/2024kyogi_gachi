61495482813729abd976f3fc74f83d552c86631d
kawakami-takeru <135945346+kawakami-takeru@users.noreply.github.com>
2024-10-19 08:26:26 +0900

    a

--- simu_main.py
+++ simu_main.py
@@ -18,8 +18,8 @@ import numpy as np
 class simu(judge.Judgec,algorithm_general.algorithm_tentative,algorithm.algorithm_tentative,algorithm_tate_ippan.algorithm_tentative):
 
     def set(self):
-        x=100
-        y=100
+        x=256
+        y=256
         n=256#一般抜き型の数
         
         first_board = np.random.randint(0, 4, (x, y))
@@ -44,13 +44,13 @@ class simu(judge.Judgec,algorithm_general.algorithm_tentative,algorithm.algorith
     
     def relord_judge_log(self):
 
-        self.start_time = time.time()#開始時間
-        self.call_algotithm=self.algo_tate(self.now_board,self.correct_board,self.use_type,self.wide,self.height)#アルゴリズム呼び出し,
-        self.end_time = time.time()#終了時間
-        self.time=self.end_time-self.start_time#かかった時間
+        # self.start_time = time.time()#開始時間
+        # self.call_algotithm=self.algo_tate(self.now_board,self.correct_board,self.use_type,self.wide,self.height)#アルゴリズム呼び出し,
+        # self.end_time = time.time()#終了時間
+        # self.time=self.end_time-self.start_time#かかった時間
 
-        print(f"general_tateは{len(self.call_algotithm)}手かかりました")
-        print(f"{self.time}秒かかりました")
+        # print(f"general_tateは{len(self.call_algotithm)}手かかりました")
+        # print(f"{self.time}秒かかりました")
 
         self.start_time = time.time()#開始時間
         self.call_algotithm_cut=self.algo_gene(self.now_board,self.correct_board,self.use_type,self.wide,self.height)#アルゴリズム呼び出し,
